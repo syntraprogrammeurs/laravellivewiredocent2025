1ca2b217b275e15343a9e37c3538eb653460c0e6
Tom <syntraprogrammeurs@gmail.com>
2025-03-22 11:57:10 +0100

    events - parent en child

---
+++ app/Livewire/ChildComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ChildComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.child-component');
+    }
+}
